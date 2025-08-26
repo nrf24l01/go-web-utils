d16b4fcfa404407188e7bdc8ce11bdc5adf7fd9d
nrf24l01 <s.nevrovsky@ya.ru>
2025-08-27 00:31:48 +0300

    Add BaseModel struct for GORM integration with UUID and timestamp fields

---
+++ pkg/goorm/base.go
@@ -0,0 +1,15 @@
+package goorm
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type BaseModel struct {
+	ID        uuid.UUID      `gorm:"type:uuid;default:gen_random_uuid();primaryKey" json:"id"`
+	CreatedAt time.Time      `gorm:"type:timestamptz;default:now()" json:"created_at"`
+	UpdatedAt time.Time      `gorm:"type:timestamptz;default:now()" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+}
