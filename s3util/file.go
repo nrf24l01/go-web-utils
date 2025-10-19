9a366787584ac5dc9bcd25f53e0ffb8ac28572b4
nrf24l01 <s.nevrovsky@ya.ru>
2025-10-19 10:39:35 +0300

    feat: add FileHash function to compute SHA256 hash of multipart file

---
+++ s3util/file.go
@@ -0,0 +1,23 @@
+package s3util
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"mime/multipart"
+)
+
+func FileHash(fileHeader *multipart.FileHeader) (string, error) {
+    file, err := fileHeader.Open()
+    if err != nil {
+        return "", err
+    }
+    defer file.Close()
+
+    hasher := sha256.New()
+    if _, err := io.Copy(hasher, file); err != nil {
+        return "", err
+    }
+
+    return hex.EncodeToString(hasher.Sum(nil)), nil
+}
