374e10181cb751a27d8302161e26ea5af1fcc88e
nrf24l01 <s.nevrovsky@ya.ru>
2025-11-01 23:21:39 +0300

    fix: update error responses in TGMiddleware to use ErrorResponse struct

--- echokit/middleware/tg_middleware.go
+++ echokit/middleware/tg_middleware.go
@@ -17,7 +17,7 @@ func TGMiddleware(config config.TgWebAppConfig) echo.MiddlewareFunc {
 			authHeader := c.Request().Header.Get("Authorization")
 			
 			if authHeader == "" {
-				return c.JSON(http.StatusUnauthorized, schemas.Error{Error: "missing token"})
+				return c.JSON(http.StatusUnauthorized, schemas.ErrorResponse{Message: "missing token", Code: http.StatusUnauthorized})
 			}
 
 			// Убираем "Bearer " из заголовка
@@ -25,24 +25,24 @@ func TGMiddleware(config config.TgWebAppConfig) echo.MiddlewareFunc {
 			if len(authHeader) > 4 && authHeader[:4] == "tma " {
 				tokenString = authHeader[4:]
 			} else {
-				return c.JSON(http.StatusUnauthorized, schemas.Error{Error: "invalid token format"})
+				return c.JSON(http.StatusUnauthorized, schemas.ErrorResponse{Message: "invalid token format", Code: http.StatusUnauthorized})
 			}
 
 			expInHour := time.Duration(config.InitDataExpireHours) * time.Hour
 
 			verifyErr := initdata.Validate(tokenString, config.TgBotToken, expInHour)
 			if verifyErr != nil {
-				return c.JSON(http.StatusUnauthorized, schemas.Error{Error: "invalid token"})
+				return c.JSON(http.StatusUnauthorized, schemas.ErrorResponse{Message: "invalid token", Code: http.StatusUnauthorized})
 			}
 			tokenData, err := initdata.Parse(tokenString)
 			if err != nil {
-				return c.JSON(http.StatusUnauthorized, schemas.Error{Error: "invalid token"})
+				return c.JSON(http.StatusUnauthorized, schemas.ErrorResponse{Message: "invalid token", Code: http.StatusUnauthorized})
 			}
 			if tokenData.User.ID != 0 {
 				c.Set("userID", tokenData.User.ID)
 				c.Set("userName", tokenData.User.Username)
 			} else {
-				return c.JSON(http.StatusUnauthorized, schemas.Error{Error: "token does not contain user data"})
+				return c.JSON(http.StatusUnauthorized, schemas.ErrorResponse{Message: "token does not contain user data", Code: http.StatusUnauthorized})
 			}
 			return next(c)
 		}
