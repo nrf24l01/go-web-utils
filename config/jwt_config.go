d9c6b00666702fc8bdb74aacdc601916fc8951c8
nrf24l01 <s.nevrovsky@ya.ru>
2025-12-03 11:42:14 +0300

    feat: refactor JWT token generation to use configuration for expiry and secrets

--- config/jwt_config.go
+++ config/jwt_config.go
@@ -9,6 +9,9 @@ import (
 type JWTConfig struct {
 	AccessJWTSecret  string `env:"ACCESS_JWT_SECRET"`
 	RefreshJWTSecret string `env:"REFRESH_JWT_SECRET"`
+
+	AccessTokenExpiryMinutes  int `env:"ACCESS_TOKEN_EXPIRY_MINUTES" envDefault:"15"`
+	RefreshTokenExpiryMinutes int `env:"REFRESH_TOKEN_EXPIRY_MINUTES" envDefault:"10080"` // 7 days
 }
 
 func LoadJWTConfigFromEnv() *JWTConfig {
