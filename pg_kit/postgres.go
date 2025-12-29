164df1f839434f0a972be06a37bbd0d57f977d0e
nrf24l01 <s.nevrovsky@ya.ru>
2025-12-29 10:34:58 +0300

    fix: enhance RegisterPostgres to configure GORM logger with slow query logging option

--- pg_kit/postgres.go
+++ pg_kit/postgres.go
@@ -3,19 +3,38 @@ package pg_kit
 import (
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/nrf24l01/go-web-utils/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
-func RegisterPostgres(cfg *config.PGConfig, models ...interface{}) (*gorm.DB, error) {
+func RegisterPostgres(cfg *config.PGConfig, noSlowSQLLogs bool, models ...interface{}) (*gorm.DB, error) {
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
 		cfg.PGHost, cfg.PGUser, cfg.PGPassword, cfg.PGDatabase, cfg.PGPort, cfg.PGSSLMode, cfg.PGTimeZone,
 	)
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// Настройка логгера GORM: отключаем лог медленных запросов, если передан флаг
+	slowThreshold := 200 * time.Millisecond
+	if noSlowSQLLogs {
+		slowThreshold = 0
+	}
+	gormLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: slowThreshold,
+			LogLevel:      logger.Warn,
+			Colorful:      false,
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: gormLogger,
+	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to get db instance: %w", err)
 	}
